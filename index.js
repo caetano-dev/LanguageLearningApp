031b794feb95f61817242c78cd9adca586035c56
drull1000 <pcaetanop@protonmail.com>
2021-10-26 19:18:20 -0300

    Bug fixes

--- index.js
+++ index.js
@@ -2,63 +2,66 @@ const textSpan = document.getElementById("textSpan");
 const button = document.getElementById("button");
 const input = document.getElementById("input");
 const answerDiv = document.getElementById("answerDiv");
+const next = document.getElementById("next");
+let sentences = [];
 
-const newPhrase = () => {
+const getPhrase = () => {
   fetch("./sentences.json")
     .then((response) => response.json())
     .then((data) => {
       const randomNumber = Math.floor(Math.random() * data.phrases.length);
-      const randomSentence = data.phrases[randomNumber];
-      const germanSentence = randomSentence.german;
-      const englishSentence = randomSentence.english;
-
-      console.log(germanSentence);
-      console.log(englishSentence);
-
-      textSpan.innerText = englishSentence;
-
-      const checkPhrase = (germanSentence) => {
-        const inputValue = input.value;
-        if (inputValue === germanSentence) {
-          textSpan.classList.add("correct");
-          textSpan.innerText = "Correct!";
-
-          setTimeout(() => {
-            textSpan.classList.remove("correct");
-            input.value = "";
-            newPhrase();
-          }, 3000);
-          return;
-        } else {
-          textSpan.classList.add("wrong");
-          textSpan.innerText = "Wrong!";
-          answerDiv.innerText = germanSentence;
-
-          setTimeout(() => {
-            answerDiv.innerText = "";
-            textSpan.classList.remove("wrong");
-            input.value = "";
-            newPhrase();
-          }, 3000);
-          return;
-        }
-      };
-
-      const handleClick = () => {
-        checkPhrase(germanSentence);
-      };
-
-      input.addEventListener("keydown", function (event) {
-        if (event.key == "Enter") {
-          event.preventDefault();
-          handleClick();
-        }
-      });
-
-      button.addEventListener("click", () => {
-        handleClick();
-      });
-    })
-    .catch((e) => console.log(e));
+      const randomPhrase = data.phrases[randomNumber];
+      const germanSentence = randomPhrase.german;
+      const englishSentence = randomPhrase.english;
+      sentences = { english: englishSentence, german: germanSentence };
+      displayPhrase();
+    });
 };
-newPhrase();
+
+const displayPhrase = () => {
+  textSpan.innerHTML = sentences.german;
+  input.value = "";
+};
+
+const checkAnswer = () => {
+  const userInput = input.value;
+
+  if (userInput === sentences.english) {
+    textSpan.classList.add("correct");
+    textSpan.innerText = "Correct!";
+    answerDiv.innerText = "Move to the next one.";
+
+    setTimeout(() => {
+      textSpan.classList.remove("correct");
+      input.value = "";
+      answerDiv.innerText = "";
+    }, 3000);
+  } else {
+    textSpan.classList.add("wrong");
+    textSpan.innerText = "Wrong!";
+    answerDiv.innerText = "Answer: " + sentences.english;
+
+    setTimeout(() => {
+      textSpan.classList.remove("wrong");
+      input.value = "";
+      answerDiv.innerText = "";
+    }, 3000);
+
+    textSpan.innerHTML = sentences.german;
+  }
+};
+
+input.addEventListener("keydown", function (event) {
+  if (event.key == "Enter") {
+    event.preventDefault();
+    checkAnswer();
+  }
+});
+button.addEventListener("click", () => {
+  checkAnswer();
+});
+next.addEventListener("click", () => {
+  getPhrase();
+});
+
+getPhrase();
